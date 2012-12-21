ae7c0cd6becc3ecf0b194ed2658f5b8a5caba9f4
Iván López <lopez.ivan@gmail.com>
2012-12-21 09:21:18 +0100

    Added script to subscribe to notifications

---
+++ create-instagram-notifications.sh
@@ -0,0 +1,295 @@
+#!/bin/bash
+
+CLIENT_ID=YOUR-INSTAGRAM-CLIENT-ID
+CLIENT_SECRET=YOUR-INSTAGRAM-CLIENT-SECRET
+CALLBACK_URL=http://XXXXX.localtunnel.com/pusheame/instagram-real-time
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=37.0625" \
+     -F "lng=-95.677068" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=33.748995" \
+     -F "lng=-84.387982" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=30.267153" \
+     -F "lng=-97.743061" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=37.871593" \
+     -F "lng=-122.272747" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=42.358431" \
+     -F "lng=-71.059773" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=41.876719" \
+     -F "lng=-87.649612" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=38.895112" \
+     -F "lng=-77.036366" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=39.737567" \
+     -F "lng=-104.984718" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=42.331427" \
+     -F "lng=-83.045754" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=29.760193" \
+     -F "lng=-95.36939" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=36.114646" \
+     -F "lng=-115.172816" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=34.052234" \
+     -F "lng=-118.243685" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=43.073052" \
+     -F "lng=-89.40123" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=25.788969" \
+     -F "lng=-80.226439" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=44.983334" \
+     -F "lng=-93.26667" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=29.951066" \
+     -F "lng=-90.071532" \
+     -F "radius=5000" \
+     -F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=40.714353" \
+     -F "lng=-74.005973" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=41.013153" \
+     -F "lng=-74.264314" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=37.441883" \
+     -F "lng=-122.14302" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=39.952335" \
+     -F "lng=-75.163789" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=40.446947" \
+     -F "lng=-59.0625" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=38.027976" \
+     -F "lng=-121.884681" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=45.523452" \
+     -F "lng=-122.676207" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=40.760779" \
+     -F "lng=-111.891047" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=29.424122" \
+     -F "lng=-98.493628" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=32.715329" \
+     -F "lng=-117.157255" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=37.77493" \
+     -F "lng=-122.419416" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=37.339386" \
+     -F "lng=-121.894956" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F "client_id=$CLIENT_ID" \
+     -F "client_secret=$CLIENT_SECRET" \
+     -F "object=geography" \
+     -F "aspect=media" \
+     -F "lat=47.60621" \
+     -F "lng=-122.332071" \
+     -F "radius=5000" \
+	-F "callback_url=$CALLBACK_URL" \
+     https://api.instagram.com/v1/subscriptions/
