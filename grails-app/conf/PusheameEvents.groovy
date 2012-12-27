4e117a827d3536169d3cd02c56ae3989e2911fc2
Iván López <lopez.ivan@gmail.com>
2012-12-27 12:03:05 +0100

    Removed unnecessary atributes in events config

--- grails-app/conf/PusheameEvents.groovy
+++ grails-app/conf/PusheameEvents.groovy
@@ -1,7 +1,7 @@
 events = {
     //'color' browser:true, scope:'*' // allows both server (scope:'app' or 'pluginName') and client (scope:'browser') to send data over this topic
     //'randomNumber' browser:true, scope:'*' // allows browser push on this topic
-    'instagramPicture' browser:true, scope:'*'
+    'instagramPicture' browser:true
     'processPicture' scope:'*'
     'timeline' browser:true, scope:'*'
 }
